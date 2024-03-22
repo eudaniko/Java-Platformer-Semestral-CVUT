b915094b5e496a5fac9758301bf4cf4824a6a506
Daniil Nikonenko <nikondan@fel.cvut.cz>
2024-03-22 14:18:14 +0100

    Add Constants and statement animation

--- src/main/java/inputs/KeyboardInputs.java
+++ src/main/java/inputs/KeyboardInputs.java
@@ -10,6 +10,9 @@ import main.GamePanel;
 import java.awt.event.KeyEvent;
 import java.awt.event.KeyListener;
 
+import static utils.Constants.Directions.*;
+
+
 
 public class KeyboardInputs implements KeyListener {
 
@@ -25,15 +28,24 @@ public class KeyboardInputs implements KeyListener {
     @Override
     public void keyPressed(KeyEvent e) {
         switch (e.getKeyCode()){
-            case KeyEvent.VK_W: gamePanel.changeYDelta(-20); break;
-            case KeyEvent.VK_A: gamePanel.changeXDelta(-20); break;
-            case KeyEvent.VK_S: gamePanel.changeYDelta(20); break;
-            case KeyEvent.VK_D: gamePanel.changeXDelta(20); break;
+            case KeyEvent.VK_W: gamePanel.setDirection(UP); break;
+            case KeyEvent.VK_A: gamePanel.setDirection(LEFT); break;
+            case KeyEvent.VK_S: gamePanel.setDirection(DOWN); break;
+            case KeyEvent.VK_D: gamePanel.setDirection(RIGHT); break;
         }
     }
 
     @Override
     public void keyReleased(KeyEvent e) {
+        switch (e.getKeyCode()){
+            case KeyEvent.VK_W:
+            case KeyEvent.VK_A:
+            case KeyEvent.VK_S:
+            case KeyEvent.VK_D:
+            default:
+                gamePanel.setMoving(false);
+                break;
+        }
 
     }
 }
--- src/main/java/main/GamePanel.java
+++ src/main/java/main/GamePanel.java
@@ -15,16 +15,31 @@ import java.awt.image.BufferedImage;
 import java.io.IOException;
 import java.io.InputStream;
 
+import static utils.Constants.PlayerConstants.*;
+import static utils.Constants.Directions.*;
+
+
 public class GamePanel extends JPanel {
 
-    private  MouseInputs mouseInputs;
-    private int xDelta = 100, yDelta = 100;
+    private MouseInputs mouseInputs;
+    private float xDelta = 100, yDelta = 100;
 
-    private BufferedImage img;
-    public GamePanel(){
+    private BufferedImage heroAtlasImg;
+    private BufferedImage[][] animations;
+    private int aniSpeed = 10;
+    private int aniTick = 0, aniIndex = 0;
+
+    private int playerAction = IDLE;
+    private int playerDir = -1;
+    private boolean moving = false;
+
+
+
+    public GamePanel() {
         mouseInputs = new MouseInputs(this);
 
-        img = importImg("/player_sprites.png");
+        heroAtlasImg = importImg("/player_sprites.png");
+        loadAnimations();
 
         setPanelSize();
         addKeyListener(new KeyboardInputs(this));
@@ -34,7 +49,6 @@ public class GamePanel extends JPanel {
 
     private BufferedImage importImg(String fileAdress) {
         InputStream is = getClass().getResourceAsStream(fileAdress);
-
         try {
             return ImageIO.read(is);
         } catch (IOException e) {
@@ -44,23 +58,79 @@ public class GamePanel extends JPanel {
 
     }
 
-    private void setPanelSize(){
-        Dimension size = new Dimension(1280,800);
+    private void loadAnimations() {
+        animations = new BufferedImage[9][6];
+
+        for (int j = 0; j < animations.length; j++) {
+            for (int i = 0; i < animations[j].length; i++) {
+                animations[j][i] = heroAtlasImg.getSubimage(i * 64, j * 40, 64, 40);
+            }
+        }
+    }
+
+    private void updateAnimationTick(){
+        aniTick++;
+        if(aniTick >= aniSpeed){
+            aniTick = 0;
+            aniIndex++;
+            if(aniIndex>= getSpriteAmount(playerAction)) aniIndex = 0;
+        }
+    }
+    public void setDirection(int direction){
+        this.playerDir =direction;
+        moving = true;
+    }
+
+    public void setMoving(boolean moving){
+        this.moving = moving;
+    }
+
+    private void setPanelSize() {
+        Dimension size = new Dimension(1280, 800);
         setMinimumSize(size);
         setPreferredSize(size);
         setMaximumSize(size);
     }
-    public void changeXDelta(int value){
+
+
+    public void changeXDelta(int value) {
         xDelta += value;
     }
 
-    public void changeYDelta(int value){
+    public void changeYDelta(int value) {
         yDelta += value;
     }
 
-    public  void paintComponent (Graphics g){
+    private void setAnimation(){
+        if(moving) playerAction = RUNNING;
+        else playerAction = IDLE;
+    }
+
+    private void updatePos(){
+        if (moving){
+            switch (playerDir){
+                case LEFT:
+                    xDelta -=5;
+                    break;
+                case UP:
+                    yDelta -=5;
+                    break;
+                case RIGHT:
+                    xDelta +=5;
+                    break;
+                case DOWN:
+                    yDelta +=5;
+                    break;
+            }
+        }
+    }
+
+    public void paintComponent(Graphics g) {
         super.paintComponent(g);
-        g.drawImage(img.getSubimage(0,0,64,40),xDelta, yDelta, 128, 80, null);
+        updateAnimationTick();
+        setAnimation();
+        updatePos();
+        g.drawImage(animations[playerAction][aniIndex], (int) xDelta, (int) yDelta, 256, 160, null);
     }
 
 }
